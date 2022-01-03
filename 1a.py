1282c0991cd623ae00eaf6261ab16e9dee9a9980
AmandeepSingh03 <67728122+AmandeepSingh03@users.noreply.github.com>
2022-01-03 16:29:51 +0530

    Add files via upload

--- 1a.py
+++ 1a.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 import numpy as np
 
 
@@ -7,27 +8,21 @@ time = []
 x = []
 y = []
 z = []
-f = open('stat_jump_stat.txt', 'r')
+f = open('linear_mov1.txt', 'r')
 for row in f:
-    row = row.split(',')
-    time.append(row[0][0:12])
-    x.append(float(row[0][-5:]))
-    y.append(float(row[1]))
-    z.append(float(row[2]))
+    row = re.split(r' |,|\n',row)
+    time.append(row[0])
+    x.append(float(row[2]))
+    y.append(float(row[3]))
+    z.append(float(row[4]))
+
 xarr = np.array(x)
 yarr = np.array(y)
 zarr = np.array(z)
-
 '''****************************'''
-# plt.title("acceleration")
-# plt.plot(time, xarr, label="x",color="red")
-# plt.plot(time, yarr, label="y",color="blue")
-# plt.plot(time, zarr, label="z",color="green")
-# plt.legend()
-# plt.show()
-rmsx=round(np.average(x),2)
-rmsy=round(np.average(y),2)
-rmsz=round(np.average(z),2)
+# rmsx=round(np.average(x),2)
+# rmsy=round(np.average(y),2)
+# rmsz=round(np.average(z),2)
 # rmsx = np.sqrt(np.mean(xarr**2))
 # rmsy = np.sqrt(np.mean(yarr**2))
 # rmsz = np.sqrt(np.mean(zarr**2))
@@ -36,17 +31,19 @@ rmsz=round(np.average(z),2)
 # yarr = yarr-rmsy 
 # zarr = zarr-rmsz  
 
-for i in range(len(xarr)-1,0,-1):
-    xarr[i]=xarr[i]-((xarr[i]+xarr[i-1])/2.0)  
-for i in range(len(yarr)-1,0,-1):
-    yarr[i]=yarr[i]-((yarr[i]+yarr[i-1])/2.0)  
-for i in range(len(zarr)-1,0,-1):
-    zarr[i]=zarr[i]-((zarr[i]+zarr[i-1])/2.0)  
-
-xarr[0]=0
-yarr[0]=0
-zarr[0]=0
-'''****************************'''
+offx=0
+offy=0
+offz=0
+for i in range(30):
+    offx=xarr[i]+offx
+offx=offx/30.0
+for i in range(30):
+    offz=zarr[i]+offz
+offz=offz/30.0
+for i in range(30):
+    offy=yarr[i]+offy
+offy=offy/30.0
+'''******************'''
 time_in_sec=[]
 for i in range(1,len(xarr)):
     time_in_sec.append(float(time[i][-9:-7])*60+float(time[i][-6:])-float(time[i-1][-9:-7])*60-float(time[i-1][-6:]))
@@ -57,8 +54,57 @@ timearr=[0]
 for i in range(1,len(time)):
     timearr.append(timearr[-1]+timeconstant)
 timearr1=np.array(timearr)
-'''********************'''
-plt.title("actual acceleration")
+
+plt.title("raw acceleration")
+plt.plot(timearr1, xarr, label="ax")
+plt.plot(timearr1, yarr, label="ay")
+plt.plot(timearr1, zarr, label="az")
+plt.legend()
+plt.show()
+
+for i in range(0,len(xarr)):
+    xarr[i]=xarr[i]-offx
+for i in range(0,len(yarr)):
+    yarr[i]=yarr[i]-offy 
+for i in range(0,len(zarr)):
+    zarr[i]=zarr[i]-offz
+
+
+plt.title("after offset acceleration")
+plt.plot(timearr1, xarr, label="ax")
+plt.plot(timearr1, yarr, label="ay")
+plt.plot(timearr1, zarr, label="az")
+plt.legend()
+plt.show()
+for i in range(len(xarr)-1,4,-1):
+    xarr[i]=((xarr[i-5]+xarr[i-1]+xarr[i-2]+xarr[i-3]+xarr[i-4])/5.0)  
+for i in range(len(yarr)-1,4,-1):
+    yarr[i]=((yarr[i-5]+yarr[i-1]+yarr[i-2]+yarr[i-3]+yarr[i-4])/5.0)  
+for i in range(len(zarr)-1,4,-1):
+    zarr[i]=((zarr[i-5]+zarr[i-1]+zarr[i-2]+zarr[i-3]+zarr[i-4])/5.0)  
+
+
+
+xarr[0]=xarr[1]=xarr[2]=xarr[3]=xarr[4]=xarr[5]
+yarr[0]=yarr[1]=yarr[2]=yarr[3]=yarr[4]=yarr[5]
+zarr[0]=zarr[1]=zarr[2]=zarr[3]=zarr[4]=zarr[5]
+
+
+# for i in range(len(xarr)-1,9,-1):
+#     xarr[i]=((xarr[i-5]+xarr[i-1]+xarr[i-2]+xarr[i-3]+xarr[i-4]+xarr[i-6]+xarr[i-7]+xarr[i-8]+xarr[i-9]+xarr[i-10])/10.0)  
+# for i in range(len(yarr)-1,9,-1):
+#     yarr[i]=((yarr[i-5]+yarr[i-1]+yarr[i-2]+yarr[i-3]+yarr[i-4]+yarr[i-6]+yarr[i-7]+yarr[i-8]+yarr[i-9]+yarr[i-10])/10.0)  
+# for i in range(len(zarr)-1,9,-1):
+#     zarr[i]=((zarr[i-5]+zarr[i-1]+zarr[i-2]+zarr[i-3]+zarr[i-4]+zarr[i-6]+zarr[i-7]+zarr[i-8]+zarr[i-9]+zarr[i-10])/10.0)  
+
+
+
+# xarr[0]=xarr[1]=xarr[2]=xarr[3]=xarr[4]=xarr[5]=xarr[6]=xarr[7]=xarr[8]=xarr[9]=xarr[10]
+# yarr[0]=yarr[1]=yarr[2]=yarr[3]=yarr[4]=yarr[5]=yarr[6]=yarr[7]=yarr[8]=yarr[9]=yarr[10]
+# zarr[0]=zarr[1]=zarr[2]=zarr[3]=zarr[4]=zarr[5]=zarr[6]=zarr[7]=zarr[8]=zarr[9]=zarr[10]
+
+
+plt.title("after average acceleration")
 plt.plot(timearr1, xarr, label="ax")
 plt.plot(timearr1, yarr, label="ay")
 plt.plot(timearr1, zarr, label="az")
@@ -96,6 +142,10 @@ plt.show()
 distancex=[0]
 distancey=[0]
 distancez=[0]
+distancex1=[0]
+distancey1=[0]
+distancez1=[0]
+
 
 for i in range(1,len(vxarr)):
     distancex.append(distancex[i-1]+(vxarr[i-1]*timediffarr[i-1])+0.5*xarr[i-1]*timediffarr[i-1]*timediffarr[i-1])
@@ -116,6 +166,16 @@ plt.plot(timearr1,dzarr,label='z')
 plt.title("displacement")
 plt.legend()
 plt.show()
+
+for i in range(1,len(vxarr)):
+    distancex1.append(distancex1[i-1]+abs((vxarr[i-1]*timediffarr[i-1])+0.5*xarr[i-1]*timediffarr[i-1]*timediffarr[i-1]))
+
+for i in range(1,len(vzarr)):
+    distancez1.append(distancez1[i-1]+abs((vzarr[i-1]*timediffarr[i-1])+0.5*zarr[i-1]*timediffarr[i-1]*timediffarr[i-1]))
+
+for i in range(1,len(vyarr)):
+    distancey1.append(distancey1[i-1]+abs((vyarr[i-1]*timediffarr[i-1])+0.5*yarr[i-1]*timediffarr[i-1]*timediffarr[i-1]))
+
 # print(timediffarr[0],timediffarr[1],timediffarr[2])
 # print(yarr[0],yarr[1],yarr[2])
 # print(vyarr[0],vyarr[1],vyarr[2])
